b79cb1851e520a47f817ddaf91c32a182ff14d86
nballelo <n.balle.lo@gmail.com>
2017-10-14 11:01:38 +0200

    full api

--- routes/parking.js
+++ routes/parking.js
@@ -1,35 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var http=require('http')
 const URL="https://api.thingtia.cloud/data";
 //var User =require('../models/users');
 var path = require('path');
 //var Hash = require('jshashes');
 
 
-router.get('/location/:location', function (req, res) {
-    Provider.find("").exec(function (err,tokens) {
-        var token=tokens[0];
+router.get('/:provider', function (req, res) {
+    Provider.findOne({name: req.params.provider}).exec(function (err, token) {
         console.log(token);
-        request( {
-            uri: URL+"/"+token.name+"/Parking_Meter1",
-            headers: {"IDENTITY_KEY":token.token,"Content-Type": "application/json"}
-        },function (error,response,body) {
-            console.log(URL+"/"+tokens[0].name+"/Parking_Meter1")
-            console.log();
-            res.send(body)
+        request({
+            uri: URL + "/" + token.name,
+            headers: {"IDENTITY_KEY": token.token, "Content-Type": "application/json"}
+        }, function (error, response, body) {
+            console.log(body);
+            res.send(body);
         })
     })
-
 });
-router.get('/allLocation', function (req, res) {
-    var NumOfParking=0;
-    res.send(NumOfParking)
+router.get('/:provider/:sensor/:number', function (req, res) {
+    Provider.findOne({name: req.params.provider}).exec(function (err, token) {
+        console.log(token);
+        request({
+            uri: URL + "/" + token.name+"?limit="+req.params.number,
+            headers: {"IDENTITY_KEY": token.token, "Content-Type": "application/json"}
+        }, function (error, response, body) {
+            console.log(body);
+            res.send(body);
+        })
+    })
 });
-
-router.get('*', function(req, res){
-    res.sendFile(path.join(__dirname, '../public/tpls', 'error.html'));
-});
-
 module.exports = router;
